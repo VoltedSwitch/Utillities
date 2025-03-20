425a834b5c5bdff308628e9fb6bbbbd504d32336
Ali Raza <wirelessvoltswitch@gmail.com>
2025-03-20 22:50:22 +0500

    example test script for instant_input function

---
+++ instant_input_func_related/example_usage_instant_input.py
@@ -0,0 +1,29 @@
+import random
+from instant_input_func import instant_input
+
+right_left_mapped = {
+    "right": "left",
+    "left": "right"
+}
+
+while True:
+    right_or_left = random.choice(["right", "left"])
+
+    print("You are in a coridoor, do you go left or right?")
+    direction = instant_input("> ", timeout=5)
+    print()
+    if direction == right_or_left:
+        print("You have fallen to your death")
+        break
+    elif direction == right_left_mapped[right_or_left]:
+        print(f"You made a run towards the {right_or_left} coridoor successfully")
+        continue
+    elif direction == "space":
+        print("You jumped over the ferocious monster")
+    elif direction == "no key pressed":
+        print("The monster devoured you")
+        break
+    else:
+        print("Ahh! You're a genius, you've won")
+        exit()
+print("The game is over, you've failed!")
